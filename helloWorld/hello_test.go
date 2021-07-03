72b5172ec6351920cfb15a9406321fa3dc7bdffc
Herchelle <herchellebotha@gmail.com>
2021-07-03 21:13:34 +0200

    minor changes

--- helloWorld/hello_test.go
+++ helloWorld/hello_test.go
@@ -6,22 +6,29 @@ func TestHello(t *testing.T){
 	
 	assertCorrectMessage := func(t testing.TB, got, want string){
 		t.Helper()
+		
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Herchelle")
+		got := Hello("Herchelle", "")
 		want := "Hello, Herchelle"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-                got := Hello("")
+                got := Hello("", "")
                 want := "Hello, World"
 
                 assertCorrectMessage(t, got, want)
         })
+
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Elodie", "spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
 }
