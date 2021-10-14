4ce7ef6ebe7fb97276bf5f0ba96c9d6d6fead23d
Herchelle Botha <herchellebotha@Herchelles-MacBook-Pro.local>
2021-10-14 20:07:55 +0200

    updated test sheet

--- arraysAndSlices/sum.go
+++ arraysAndSlices/sum.go
@@ -1,11 +1,10 @@
-package main
+package arraysAndSlices
 
-func Sum(numbers []int) int {
-	sum := 0
-
-	for _, number := range numbers {
-		sum += number
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
 
-	return sum
+	return sums
 }
